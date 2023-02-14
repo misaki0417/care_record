13bdc0777b5c9a662582feefcc6a30db71f53265
misaki0417 <110611390+misaki0417@users.noreply.github.com>
2023-02-14 15:19:39 +0000

    Add Bootstrapインストール

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -71,6 +71,7 @@ Rails.application.configure do
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
   config.hosts << "65751a1945bc4fc4a4be46bd9b70eb90.vfs.cloud9.ap-northeast-1.amazonaws.com"
+  config.web_console.whitelisted_ips ='180.146.165.70'
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
